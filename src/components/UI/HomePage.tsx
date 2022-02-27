b5632712fe667db3477a1fd2ca14a1803ff0d6e6
Dekguh <kadekteguhmahesa@gmail.com>
2022-02-27 13:42:27 +0800

    remove

--- src/components/UI/HomePage.tsx
+++ src/components/UI/HomePage.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client'
-import LoadingScreen from './LoadingScreen'
-import Input from './control/Input'
 import withAuth from '../utils/HOC/withAuth'
 
-const POST_QUERY = gql`
-    query {
-        artikel(id: 1) {
-            id
-            judul
-            konten
-        }
-    }
-`
-
 const HomePage = () => {
-    const { data, loading, error } = useQuery(POST_QUERY)
-
-    if(loading) return(<LoadingScreen />)
-
-    console.log(data)
 
     return (
     <>
         <div>
-            <Input />
+            home
         </div>
     </>
     )
